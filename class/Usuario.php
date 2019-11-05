5cb71ae997e646ed67873d0e860c61d0dc5e3f18
Brodinho <vepnet@gmail.com>
2019-11-05 14:57:18 -0300

    Metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -76,6 +76,56 @@
 
 		}
 
+		public static function getList()
+		{
+
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_usuarios order by idusuario");
+		}
+
+
+		public static function search($login)
+		{
+
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY idusuario", array(
+				 ':SEARCH'=>"%".$login."%"
+			));
+		}
+
+		public function login($login, $password)
+		{
+
+			$sql = new Sql();
+
+			$result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD",array(
+				":LOGIN"=>$login,
+				":PASSWORD"=>$password
+
+			));
+
+
+
+			if(count($result) > 0)
+			{
+				$row = $result[0];
+
+				$this->setIdusuario($row['idusuario']);
+				$this->setDeslogin($row['deslogin']);
+				$this->setDessenha($row['dessenha']);
+				$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+			}
+			else
+			{
+
+				throw new Exception("Login e/ou senha inválidos");
+			
+			}
+		}
+
 		public function __toString()
 		{
 			return json_encode(array(
@@ -84,7 +134,7 @@
 				"dessenha"=>$this->getDessenha(),
 				"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
 			));
-		}
+		} 
 
 	} // Fim da Classe Usuario
 
--- index.php
+++ index.php
@@ -2,13 +2,27 @@
 
 	require_once("config.php");
 
+//	Carrega um usuario usuando o login e senha
+	$usuario = new Usuario();
+	$usuario->login("user1","1234");
+
+	echo $usuario;
+
+// Carrega uma lista de usuários buscando pelo login
+//	$search = Usuario::search("se");
+//	echo json_encode($search);
+
+//	Carrega uma lista de usuários
+//	$lista = Usuario::getList();
+//	echo json_encode($lista);
+
+
+/*  Rotina para carregar apenas um usuário
 	$user = new Usuario();
-
-	$user->loadById(3);
-	
+	$user->loadById(3);	
 	echo $user;
+-------------------------------------------
 
-/*
 	- Código antigo
 	$sql = new Sql();
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
